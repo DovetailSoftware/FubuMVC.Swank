7b284ab3af063dfa95b764bd768e103d5bc7eb03
chad.myers <chad.myers@gmail.com>
2017-03-30 23:36:30 -0500

    Restore assembly-pak stuff  - fix Nuspec to be in line with previous nuspec  - Remove web.config, shouldn't have been there

--- rakefile.rb
+++ rakefile.rb
@@ -7,8 +7,15 @@ BUILD_NUMBER = build_number
 msbuild = '"C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe"'
 
 desc 'Compile the code'
-task :compile => [:clean, :version] do
+task :compile => [:clean, :version, :pack_bottle] do
   sh "#{msbuild} src/FubuMVC.Swank.sln /property:Configuration=#{COMPILE_TARGET} /v:m /t:rebuild /nr:False /maxcpucount:8"
+end
+
+desc "Create the assembly-pak bottles file"
+task :pack_bottle do
+  toolPath = get_nuget_tool_path("Bottles", "BottleRunner.exe")
+  fullCmd "#{toolPath} assembly-pak src/Swank -p Swank.csproj"
+  sh fullCmd
 end
 
 desc "Update the version information for the build"
@@ -26,7 +33,7 @@ task :version do
   options = {
     :description => 'FubuMVC.Swank',
     :product_name => 'FubuMVC.Swank',
-    :copyright => 'Copyright (c) 2013 Ultraviolet Catastrophe',
+    :copyright => "Copyright (c) 2013-#{Time.now.year} Ultraviolet Catastrophe",
     :company => 'Ultraviolet Catastrophe',
     :trademark => commit,
     :version => asm_version,
@@ -62,4 +69,10 @@ end
 desc 'Run the unit tests without compile'
 task :fast_test do
   sh "src/packages/NUnit.ConsoleRunner.3.6.1/tools/nunit3-console.exe src/Tests/bin/#{COMPILE_TARGET}/Tests.dll"
+end
+
+def get_nuget_tool_path(name, tool)
+    path = Dir.glob("**/packages/#{name}**/tools/#{tool}")
+    fail "#{name} nuget tool #{tool} could not be found under #{Dir.getwd}." unless !path.empty?
+    return path.sort {|x,y| y <=> x } [0]
 end
